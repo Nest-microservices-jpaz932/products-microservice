9e127addba539511ee8e9b9eafed024e9c3959cd
jpaz932 <juanpa.aristis@gmail.com>
2025-02-04 00:40:44 -0500

    refactor(products): clean up controller methods and improve error handling in service

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,4 @@
-import {
-    Controller,
-    // Get,
-    // Post,
-    // Body,
-    // Patch,
-    // Param,
-    // Delete,
-    // Query,
-    ParseIntPipe,
-} from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -19,38 +9,29 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 export class ProductsController {
     constructor(private readonly productsService: ProductsService) {}
 
-    // @Post()
     @MessagePattern({ cmd: 'createProduct' })
     create(@Payload() createProductDto: CreateProductDto) {
         return this.productsService.create(createProductDto);
     }
 
-    // @Get()
     @MessagePattern({ cmd: 'findAllProducts' })
     findAll(@Payload() paginationDto: PaginationDto) {
         return this.productsService.findAll(paginationDto);
     }
 
-    // @Get(':id')
     @MessagePattern({ cmd: 'findOneProduct' })
     findOne(@Payload('id', ParseIntPipe) id: number) {
         return this.productsService.findOne(id);
     }
 
-    // @Patch(':id')
     @MessagePattern({ cmd: 'updateProduct' })
-    update(
-        // @Param('id') id: string,
-        // @Body() updateProductDto: UpdateProductDto,
-        @Payload() updateProductDto: UpdateProductDto,
-    ) {
+    update(@Payload() updateProductDto: UpdateProductDto) {
         return this.productsService.update(
             updateProductDto.id,
             updateProductDto,
         );
     }
 
-    // @Delete(':id')
     @MessagePattern({ cmd: 'deleteProduct' })
     remove(@Payload('id') id: string) {
         return this.productsService.remove(+id);
