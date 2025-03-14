6b7d879339f5e8c853a4556c819b730ab4d626da
jpaz932 <juanpa.aristis@gmail.com>
2025-03-13 20:42:49 -0500

    feat(nats): integrate NATS for microservice communication and update environment configuration

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,17 +4,22 @@ import * as joi from 'joi';
 interface EnvVars {
     PORT: number;
     DATABASE_URL: string;
+    NATS_SERVERS: string[];
 }
 
 const envSchema = joi
     .object({
         PORT: joi.number().required(),
         DATABASE_URL: joi.string().required(),
+        NATS_SERVERS: joi.array().items(joi.string()).required(),
     })
     .unknown(true);
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const { error, value } = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+    ...process.env,
+    NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
+});
 
 if (error) {
     throw new Error(`Config validation error: ${error.message}`);
@@ -22,9 +27,14 @@ if (error) {
 
 const envVars: EnvVars = value as EnvVars;
 
-const { PORT: port = 3000, DATABASE_URL: databaseUrl } = envVars;
+const {
+    PORT: port = 3000,
+    DATABASE_URL: databaseUrl,
+    NATS_SERVERS: natsServers,
+} = envVars;
 
 export const envs = {
     port,
     databaseUrl,
+    natsServers,
 };
